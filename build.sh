62491aaf93b4239ec261c1b5a5560e0eb7bbbf3a
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-09 13:37:30 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ apt install -y libx11-dev libxft-dev libxext-dev libharfbuzz-dev libxinerama-dev
 git clone --depth=1 https://github.com/kzwkt/instantWM
 cd instantWM
 make
-make DESTDIR="./dwmpkg" install
+make DESTDIR="$PWD/dwmpkg" install
 zip -r dwmpkg.zip dwmpkg
