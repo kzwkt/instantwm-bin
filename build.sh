c3679f86381425e4cad909157325b684b42aaa26
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-09 13:16:45 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 apt update -y
 apt install -y libx11-dev libxft-dev libxext-dev libharfbuzz-dev libxinerama-dev zip git gcc make 
 git clone --depth=1 https://github.com/kzwkt/instantWM
-cd st-instantos
+cd instantWM
 make
 make PREFIX="./dwmpkg" install
 zip -r dwmpkg.zip dwmpkg
