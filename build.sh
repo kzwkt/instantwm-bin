03be3f68137f95f45e929634c485cfad69af7c1b
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-09 13:51:34 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 apt update -y
-apt install -y libx11-dev libxft-dev libxext-dev libharfbuzz-dev libxinerama-dev zip git gcc make  sed 
+apt install -y libx11-dev libxft-dev  libxinerama-dev zip git gcc make  sed 
 git clone --depth=1 https://github.com/kzwkt/instantWM
 cd instantWM
 make
 mkdir -p ./dwmpkg/usr/bin
+mkdir -p ./dwmpkg/usr/share/man/man1/
 make PREFIX=/usr DESTDIR="./dwmpkg" install
 zip -r dwmpkg.zip dwmpkg
