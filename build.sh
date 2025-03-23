f57e9359057bd5b96c45fd95b8c0b83df167722f
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-23 11:08:28 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 ###
 apt update -y
-apt install -y libx11-dev libxft-dev  libxinerama-dev  git gcc make  sed ca-certificates --no-install-recommends
+apt install -y libx11-dev libxft-dev  libxinerama-dev  git gcc make  sed ca-certificates binutils --no-install-recommends
 git clone --depth=1 https://github.com/kzwkt/instantWM
 cd instantWM
 make
+strip instantwm
